7508a8c2d1b3a27a029fed26dbc4cf72ab3492ff
Florencia De Simone <mfadesimone@gmail.com>
2025-11-12 13:08:51 -0300

    taream2u1

---
+++ Tarea m2u1/taream2u1.js
@@ -0,0 +1,17 @@
+function suma(cuatrimA, cuatrimB) {
+    return (cuatrimA + cuatrimB)
+}
+
+const resultado = suma(8, 9)
+
+////console.log(resultado)
+
+function promedio(resultado, cantidadcuatrim) {
+    return (resultado / cantidadcuatrim)
+}
+
+const cantidadcuatrim = 2
+
+const promediohistoria = resultado/cantidadcuatrim
+
+console.log(promediohistoria)
